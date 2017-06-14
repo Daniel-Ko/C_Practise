e312b0c6f5d0f6edb5850637418d5ae7f5738469
Daniel-Ko <daniel.j.ko@gmail.com>
2017-06-14 18:12:24 +1200

    Create increment_operators_in_preprocessorFunctions.c

--- increment_operators_in_preprocessorFunctions.c
+++ increment_operators_in_preprocessorFunctions.c
@@ -5,7 +5,7 @@ int main()
 {
     int x = 0;
     
-    //0 (x->1) * 1 (x->2) = 0
+    //0 (x->1) * 1 = 0, (x->2)
     int post = f(x++);
     printf("After postincrements, x = %d\n", x); //x = 2
     x = 0;
@@ -13,7 +13,7 @@ int main()
     int norm = f(x); //x = 0
     printf("After a normal call,  x = %d\n", x);
     
-    //(x->1) 1 * (x->2) = 
+    //(x->1) 1 * (x->2) ..this has the effect of making 2 * 2, I think??
     int pre = f(++x);
     printf("After preincrements,  x = %d\n", x); //x = 2
     
